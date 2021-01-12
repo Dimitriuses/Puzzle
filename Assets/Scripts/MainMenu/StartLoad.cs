206b7f500784922ea9ff9f412466da029cf1eb0e
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2021-01-12 18:05:34 +0200

    start new project (-3day to push it)

---
+++ Assets/Scripts/MainMenu/StartLoad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartLoad : MonoBehaviour
+{
+    public Image ResizeControlImage;
+
+    public TextMeshProUGUI UperText;
+    public TextMeshProUGUI CenterText;
+    public TextMeshProUGUI DownerText;
+
+    public int LoadStatus;
+    public bool StartBios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

64cf3d80471026074bbc3054654c6ec4de82b75a
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2021-01-12 19:31:26 +0200

    firs script (dont work but don t wory)

--- Assets/Scripts/MainMenu/StartLoad.cs
+++ Assets/Scripts/MainMenu/StartLoad.cs
@@ -7,17 +7,30 @@ using UnityEngine.UI;
 public class StartLoad : MonoBehaviour
 {
     public Image ResizeControlImage;
-
+    public Sprite Sprite;
     public TextMeshProUGUI UperText;
+    public RectTransform UperTextRectTransform;
     public TextMeshProUGUI CenterText;
     public TextMeshProUGUI DownerText;
 
     public int LoadStatus;
     public bool StartBios;
+    public float FountSize;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    private void OnRectTransformDimensionsChange()
+    {
+        float Iwidth = ResizeControlImage.rectTransform.rect.width;
+        float Iheight = Iwidth / Sprite.rect.width * Sprite.rect.height;
+        //Debug.Log(Iwidth + " " + Iheight);
+        //UperTextRectTransform.rect.Set(UperTextRectTransform.rect.x, UperTextRectTransform.rect.y, UperTextRectTransform.rect.width, Iheight);
+        //UperTextRectTransform.sizeDelta.Set(UperTextRectTransform.sizeDelta.x, Iheight);
     }
 
     // Update is called once per frame

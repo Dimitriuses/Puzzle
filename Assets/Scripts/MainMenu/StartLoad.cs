e56281a107f40c3b1530ff1bd64af6781bc80c0b
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2021-01-13 19:13:13 +0200

    Start load complete

--- Assets/Scripts/MainMenu/StartLoad.cs
+++ Assets/Scripts/MainMenu/StartLoad.cs
@@ -7,20 +7,28 @@ using UnityEngine.UI;
 
 public class StartLoad : MonoBehaviour
 {
+    [Header("Resize settings")]
     public Image ResizeControlImage;
     public Sprite Sprite;
     public TextMeshProUGUI UperText;
     public RectTransform UperTextRectTransform;
     public TextMeshProUGUI CenterText;
     public TextMeshProUGUI DownerText;
+    public float FontSize;
+
+    [Header("Animation atributes")]
     [Range(0, 100)]
     public int LoadStatus;
     [Range(0, 100)]
     public int memTestStatus;
     [Range(0, 100)]
     public int IDEDetectStatus;
+
+    [Header("Link Atributes")]
     public bool StartBios;
-    public float FontSize;
+    public GameObject StartLoadPanel;
+    public GameObject BIOSPanel;
+    public GameObject GRUBPanel;
 
     int memSize = 4194304;
     string Info;
@@ -28,6 +36,9 @@ public class StartLoad : MonoBehaviour
     string MemTest;
     string DetectIDEvice;
     string DetectedDevices;
+
+    string VGAInfo;
+    string keyButonInfo;
     
     // Start is called before the first frame update
     void Start()
@@ -41,6 +52,8 @@ public class StartLoad : MonoBehaviour
             "<Br>" + "   Prymary Slave  : SAMSUNG SP4002H QU100-60" +
             "<Br>" + "Secondary Master  : Pioneer DVD-ROM ATAPIModel" +
             "<Br>" + " Secondary Slave  : SAMSUNG CF/ATA 04/05/06";
+        VGAInfo = "08/04/2004-nVidia-nForce-A7N8X2.0";
+        keyButonInfo = "Press <color=white> DEL </color> to enter SETUP";
         //UperText.onCullStateChanged.AddListener(delegate { OnFontChange(); });
         FontSize = UperText.fontSize;
         OnRectTransformDimensionsChange();
@@ -64,6 +77,20 @@ public class StartLoad : MonoBehaviour
         DownerText.fontSize = FontSize;
     }
 
+    void TimeIsUp()
+    {
+        StartLoadPanel.SetActive(false);
+        if (StartBios)
+        {
+            BIOSPanel.SetActive(true);
+            StartBios = false;
+        }
+        else
+        {
+            GRUBPanel.SetActive(true);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -71,13 +98,27 @@ public class StartLoad : MonoBehaviour
         {
             OnFontChange();
         }
+
+        if (Input.GetKey(KeyCode.Delete))
+        {
+            StartBios = true;
+        }
+
         //float LTMP = (LoadStatus - 15) * ((50 - 15) / 100);
         //Debug.Log(LTMP);
         //memTestStatus = (LoadStatus >= 15 && LoadStatus <= 50) ?(int)LTMP: (LoadStatus < 15) ? 0 : 100;
-        string tmpText = ((LoadStatus <= 5) ? "" : Info) + "<Br><Br>" + ((LoadStatus >=10)?ProcInfo:"") + "<Br>" +
+        string tmpCText = ((LoadStatus <= 5) ? "" : Info) + "<Br><Br>" + ((LoadStatus >=10)?ProcInfo:"") + "<Br>" +
         ((memTestStatus > 0) ? MemTest + " " + memSize / 100 * memTestStatus + " " + ((memTestStatus >= 100) ? "OK" : "") : "") + "<Br><Br>" +
         ((IDEDetectStatus > 0) ? DetectIDEvice : "") + ((IDEDetectStatus > 10 && IDEDetectStatus < 80) ? "Detecting IDE device ..." : "") + ((IDEDetectStatus >= 80) ? DetectedDevices : "");
 
-        CenterText.text = tmpText;
+        string tmpDText = keyButonInfo + ((memTestStatus>0)? ", <color=white>ESC</color> to skip" :" ") + "<Br>" + VGAInfo;
+        
+        CenterText.text = tmpCText;
+        DownerText.text = tmpDText;
+        
+        if(LoadStatus >= 100 || (memTestStatus>0 && Input.GetKey(KeyCode.Escape)))
+        {
+            TimeIsUp();
+        }
     }
 }

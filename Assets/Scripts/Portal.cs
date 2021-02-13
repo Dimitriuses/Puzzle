56bb91d36c4558e10a5edb762cf82d4dfa65475b
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2021-02-13 15:54:53 +0200

    not so good but we go

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Transform PlayerTransform;
-    public Transform PortalCanwasTransform;
+    public Transform OtherPortalCanwasTransform;
     public Camera Camera;
     public Transform TEST;
+    private Transform PortalCanwasTransform;
     // Start is called before the first frame update
     void Start()
     {
-
+        PortalCanwasTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(PlayerTransform.position - PortalCanwasTransform.position);
-        Camera.transform.localPosition = (PlayerTransform.position - PortalCanwasTransform.position) * -2;
+        Vector3 PVecPos = PlayerTransform.position;
+        Vector3 PCVecPos = PortalCanwasTransform.position;
+        Vector3 DCLPos = (PVecPos - PCVecPos) * -2;
+        DCLPos.y = PVecPos.y;
+        Camera.transform.localPosition = DCLPos;
         Vector2 a = new Vector2(PlayerTransform.position.x, PlayerTransform.position.z);
         Vector2 b = new Vector2(PortalCanwasTransform.position.x, PortalCanwasTransform.position.z);
         float c = Mathf.Sqrt(Mathf.Pow(b.x - a.x, 2) + Mathf.Pow(b.y - a.y, 2));
         //Debug.Log(c);
-        //TEST.localPosition = (PlayerTransform.position - PortalCanwasTransform.position) * -2;
-        //TEST.eulerAngles = (PlayerTransform.eulerAngles + new Vector3(0, -90, 0));
-        Camera.transform.eulerAngles = (PlayerTransform.eulerAngles + new Vector3(0, -90, 0));
-        Camera.focalLength = 30.0f + c;
+        TEST.localPosition = (PlayerTransform.position - PortalCanwasTransform.position) * -2;
+        //float F = Mathf.Atan2(TEST.localPosition.z, TEST.localPosition.x);
+        //Debug.Log(TEST.localPosition.x + " " + TEST.localPosition.z + " F=" + F);
+        //TEST.eulerAngles = (PlayerTransform.eulerAngles + new Vector3(0, F, 0));
+        //TEST.eulerAngles = new Vector3(0, F, 0);
+        TEST.LookAt(OtherPortalCanwasTransform);
+        //Camera.transform.eulerAngles = (PlayerTransform.eulerAngles + new Vector3(0, -90, 0));
+        Camera.transform.LookAt(OtherPortalCanwasTransform);
+        Camera.transform.eulerAngles = new Vector3(0, Camera.transform.eulerAngles.y, 0);
+        Camera.focalLength = 30.0f + c*2;
     }
 }
